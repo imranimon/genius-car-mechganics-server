ea67076e89f0ab15965d800256bf0acb0149ef0b
Imran <imranimon1@gmail.com>
2021-10-26 22:31:37 +0200

    something changed

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// user: mydbuser1
-// password: ArfCI2fhVh7YWWZJ
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ug28d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
